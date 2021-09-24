84ca9c411459434da670d3a9e1be804e0c5034c5
sarathkrishnan2000 <78251226+sarathkrishnan2000@users.noreply.github.com>
2021-09-24 21:45:35 +0530

    Add files via upload

---
+++ Lab 5/graphics/figarea.java
@@ -0,0 +1,11 @@
+package graphics;
+
+public class figarea{
+    public static void main(String[] args) {
+        fig f = new fig();
+        f.tri();
+        f.rect();
+        f.sqr();
+        f.cir();
+    }
+}
